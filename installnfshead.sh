a65c23470a360341586b690556fc08098d9188ba
Nik Eastburn <nik@ndeast.com>
2018-10-16 20:20:28 -0400

    change exports to append

--- installnfshead.sh
+++ installnfshead.sh
@@ -5,12 +5,12 @@ mkdir /software
 chmod -R 755 /software
 
 for i in {4..15}; do
-    echo "/software 192.168.1.$i(rw,sync,no_root_squash,no_all_squash)" > /etc/exports
+    echo "/software 192.168.1.$i(rw,sync,no_root_squash,no_all_squash)" >> /etc/exports
 done
 
 systemctl restart nfs-server
 
 mkdir -p /mnt/nfs/scratch
 mount -t nfs 192.168.1.3:/scratch /mnt/nfs/scratch
-touch /mnt/nfs/software/test_nfs_file_head_scratch
+touch /mnt/nfs/scratch/test_nfs_file_head_scratch
 
