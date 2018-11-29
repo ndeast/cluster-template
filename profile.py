67abe5bb758ae6b3a41d3a9fbec0f3de43bb8289
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-11-29 17:57:23 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -36,7 +36,7 @@ prefixForIP = "192.168.1."
 
 link = request.LAN("lan")
 
-for i in range(15):
+for i in range(3):
   if i == 0:
     node = request.XenVM("head")
     node.routable_control_ip = "true"
