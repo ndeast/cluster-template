d521c9aecfd00665ef8a57f649cd469d2be5d99f
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-12-14 12:02:57 -0500

    Update and rename createsql.sql to createsql.sh

---
+++ createsql.sh
@@ -0,0 +1,9 @@
+#https://slurm.schedmd.com/accounting.html
+
+create database slurm_acct_db;
+create user 'slurm'@'localhost';
+set password for 'slurm'@'localhost' = password('password');
+grant usage on *.* to 'slurm'@'localhost';
+grant all privileges on slurm_acct_db.* to 'slurm'@'localhost';
+flush privileges;
+exit
--- createsql.sql
+++
@@ -1,3 +0,0 @@
-grant all on slurm_acct_db.* TO 'slurm'@'localhost' identified by 'secretkey' with grant option;
-create database slurm_acct_db;
-quit;
