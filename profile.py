ea6bb03403a565c657778c749fe36aa9039445b0
bchung831 <43188967+bchung831@users.noreply.github.com>
2018-12-14 12:13:14 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -51,6 +51,7 @@ for i in range(6):
     node.addService(pg.Execute(shell="sh", command="sudo yum -y install nfs-utils"))
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/mapdrivestometa.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/mapdrivestometa.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo cp /local/repository/createsql.sh /scratc/createsql.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/installmunge_meta.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/installmunge_meta.sh"))
 
